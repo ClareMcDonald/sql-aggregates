c296bd82da9992faef9befb653380f04b64abed8
ClareMcDonald <clare.s.mcdonald@gmail.com>
2022-03-23 13:02:06 -0700

    work on exercise 2

--- exercise-2/exercise-2.sql
+++ exercise-2/exercise-2.sql
@@ -1,3 +1,14 @@
 -- the top 10 cities with the most total rental payment amount
+SELECT
+    city
+    COUNT(*) as count
+FROM    
+    city
+INNER JOIN
+    address
+ON
+    city.city_id = address.city_id
+customer
+    add
 -- include the city name and sum
 -- use SUM to sum up the amounts
